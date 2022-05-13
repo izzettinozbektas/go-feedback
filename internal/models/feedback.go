5073fb8fef75fff0c06f79e3c39e89b259701cc0
izzettinozbektas <izzettinozbektas54@gmail.com>
2022-05-14 01:02:48 +0300

    object id modele eklendi.

--- internal/models/feedback.go
+++ internal/models/feedback.go
@@ -1,10 +1,12 @@
 package models
 
 import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"time"
 )
 
 type Feedback struct {
-	Feedback string `json:"feedback"`
-	Created_at time.Time `json:"created_at" bson:"created_at"`
+	Id         primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Feedback   string             `json:"feedback"`
+	Created_at time.Time          `json:"created_at" bson:"created_at"`
 }
